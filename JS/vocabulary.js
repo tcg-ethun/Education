9649f9fe3c7af0eb8ba11f1de3a839306cb88d80
tcg-ethun <ethuntcg@gmail.com>
2025-08-01 09:53:14 +0600

    Updated

--- JS/vocabulary.js
+++ JS/vocabulary.js
@@ -1,69 +1,194 @@
 
- const banglaFacts = [
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Zodiac</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>   রাশিচক্র </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Sabotage </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> অন্তর্ঘাত  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Kingdom </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> রাজ্য  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Will  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> ইচ্ছাপত্র </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Realism</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> বাস্তববাদ </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Oath </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> শপথ  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Nomination</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  মনোনয়ন</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Liberal </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> উদার  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Jail-Code </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  কারাবিধি </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>Immigrant   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  অভিবাসী</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Globalisation </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> বিশ্বায়ন  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Geology </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> ভূতত্ত্ব  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Fiction</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> কথাসাহিত্য </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   Era</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  যুগ </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Biography </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  জীবনি </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Ability</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  সামর্থ্য</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   Gain</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  অর্জন </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Myth </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> উদ্ভিদ  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Motion </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>   গতি</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Pole </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  মরু </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Orbit </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  কক্ষ</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Debt </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  ঋণ </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Capital </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  মূলধন  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Deed </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  দলিল </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Civil </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> অসামরিক  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Valid  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>বৈধ    </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Global </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>   বৈশ্বিক</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Rank </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>পদমর্যাদা   </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Legend </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>কিংবদন্তী   </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Sub-Law  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> উপ আইন   </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Forecast </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> পূর্বাভাস  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Post-Paid </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> পরে প্রদত্ত</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Surplus </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>উদ্বৃত্ত  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   Bookpost</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> খোলাডাক </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   Stamp</span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>সিলমোহর   </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Deed of Gift  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  দানপত্র  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Embargo </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  নিষেধাজ্ঞা</span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Terminology </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> পরিভাষা  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>  Up-To-Date </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> হালনাগাদ </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Impeachment  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>অভিশংসন  </span> ",
-    "<span style='color: blue; font-size:20px; font-family:Fira Sans;'> Lien  </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'> পূর্বস্বত্ব </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-    // "<span style='color: blue; font-size:20px; font-family:Fira Sans;'>   </span> <br> <i class='fa-solid fa-arrow-down'></i> <br> <span style=' font-size:20px;'>  </span> ",
-];
+
+    const wordPairsData = [
+      [
+  { bangla: "প্রত্যাখ্যান করা", english: "reject" },
+{ bangla: "চালিয়ে যাওয়া", english: "continue" },
+{ bangla: "পদ ছেড়ে দেয়া", english: "abdicate" },
+   { bangla: " তিব্রতর করা", english: "intensify" },
+{ bangla: " প্রশমিত করা", english: "mitigate" },
 
 
+      ],
+      [
+  { bangla: "পরিত্যাগ করা", english: "forsake" },
+{ bangla: " বহাল রাখা", english: "uphold" },
+{ bangla: "ত্যাগ করা", english: "give up" },
+   { bangla: " বৃদ্ধি করা ", english: "amplify" },
 
-  function getBanglaFact() {
-    const randomIndex = Math.floor(Math.random() * banglaFacts.length);
-    return banglaFacts[randomIndex];
+{ bangla: "সমর্থন দেয়া", english: "espouse" },
+      ],
+      [
+          { bangla: " ধরে রাখা", english: " retain" },
+{ bangla: " আলিঙ্গন করা", english: "embrace" },
+{ bangla: "এড়িয়ে চলা", english: "shun" },
+{ bangla: "কোনো কিছুকে গ্রহণ করা", english: "adopt" },
+{ bangla: " হ্রাস করা", english: "decline" },
+
+      ],
+      [
+          { bangla: " কোনো কিছুর পিছনে ছোটা", english: "pursue" },
+{ bangla: "কমে যাওয়া", english: " diminish" },
+{ bangla: "ত্যাগ করা", english: "quit" },
+{ bangla: " দীর্ঘায়িত করা", english: "prolong" },
+{ bangla: "ছুড়ে ফেলা", english: "discard" },
+
+      ],
+      [
+   { bangla: "ফেলে যাওয়া", english: "desert" },
+   { bangla: "ছেড়ে দেওয়া", english: "relinquish" },
+   { bangla: "আরো খারাপ করা", english: "make worse" },
+   { bangla: "বাড়িয়ে দেয়া", english: "boost" },
+   { bangla: "কমে যাওয়া", english: "dwindle" },
+      ],
+
+      [
+
+   { bangla: " কমানো", english: "lessen" },
+   { bangla: "বাড়িয়ে দেয়া", english: "magnify" },
+{ bangla: "অস্বীকার করা", english: "renounce" },
+   { bangla: " অত্যন্ত রাগানো ", english: " exasperate" },
+{ bangla: "ত্যাগ করা", english: "give up" },
+      ],
+  
+    ];
+
+let currentWordPairs = [];
+let availableEnglishWords = [];
+let userAnswers = {};
+
+const quizContainer = document.getElementById('quiz-container');
+const pairsContainer = document.getElementById('pairs-container');
+const englishWordsContainer = document.getElementById('english-words-container');
+const roundResultDiv = document.getElementById('round-result');
+const solutionContainer = document.getElementById('solution-container');
+const solutionList = solutionContainer.querySelector('.solution-list');
+
+// Shuffle function
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+}
 
-  const factElement = document.getElementById("banglaFact");
-  factElement.innerHTML = getBanglaFact(); 
+function initializeQuiz() {
+  currentWordPairs = wordPairsData[Math.floor(Math.random() * wordPairsData.length)];
+  availableEnglishWords = currentWordPairs.map(pair => pair.english);
 
-  document.getElementById("newBanglaFactButton").addEventListener("click", () => {
-    factElement.innerHTML = getBanglaFact();
+  shuffleArray(availableEnglishWords);
+
+  userAnswers = {};
+
+  roundResultDiv.textContent = '';
+  solutionContainer.style.display = 'none';
+  quizContainer.style.display = 'flex';
+
+  generateQuizUI();
+}
+
+function generateQuizUI() {
+  pairsContainer.innerHTML = '';
+  englishWordsContainer.innerHTML = '';
+
+  currentWordPairs.forEach(pair => {
+    const wordPairDiv = document.createElement('div');
+    wordPairDiv.classList.add('word-pair');
+    wordPairDiv.innerHTML = `
+      <div class="bangla">${pair.bangla}</div>
+      <div class="drop-zone" data-bangla="${pair.bangla}" onclick="selectDropZone(this)"></div>
+    `;
+    pairsContainer.appendChild(wordPairDiv);
   });
 
+  availableEnglishWords.forEach(word => {
+    const div = document.createElement('div');
+    div.classList.add('english-word');
+    div.textContent = word;
+    div.onclick = () => selectWord(word, div);
+    englishWordsContainer.appendChild(div);
+  });
+
+  selectedDropZone = null;
+}
+
+let selectedDropZone = null;
+
+function selectDropZone(zone) {
+  selectedDropZone = zone;
+  // Highlight selected drop zone
+  document.querySelectorAll('.drop-zone').forEach(z => z.style.borderColor = '#ccc');
+  zone.style.borderColor = '#3a6df0';
+}
+
+function selectWord(word, element) {
+  if (!selectedDropZone) {
+    alert('প্রথমে একটি বাংলা শব্দের বক্সে ক্লিক করুন যেখানে আপনি ইংরেজি শব্দ বসাতে চান।');
+    return;
+  }
+
+  // পুরানো শব্দ থাকলে সাজেশনে ফেরত পাঠাও
+  const oldWord = selectedDropZone.textContent.trim();
+  if (oldWord) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('english-word');
+    newDiv.textContent = oldWord;
+    newDiv.onclick = () => selectWord(oldWord, newDiv);
+    englishWordsContainer.appendChild(newDiv);
+  }
+
+  selectedDropZone.textContent = word;
+  selectedDropZone.style.borderColor = '#3a6df0';
+
+  const banglaWord = selectedDropZone.getAttribute('data-bangla');
+  userAnswers[banglaWord] = word;
+
+  element.remove();
+
+  selectedDropZone = null;
+  document.querySelectorAll('.drop-zone').forEach(z => z.style.borderColor = '#ccc');
+
+  checkIfComplete();
+}
+
+function checkIfComplete() {
+  const dropZones = document.querySelectorAll('.drop-zone');
+  let allFilled = true;
+  dropZones.forEach(zone => {
+    if (!zone.textContent.trim()) allFilled = false;
+  });
+
+  if (allFilled) {
+    showResult();
+  }
+}
+
+function showResult() {
+  quizContainer.style.display = 'none';
+  solutionContainer.style.display = 'block';
+  roundResultDiv.textContent = 'আপনার ফলাফল নিচে দেখুন';
+
+  solutionList.innerHTML = '';
+
+  // currentWordPairs এর কপি তৈরি করে সেটাকে শাফল করবো
+  const shuffledPairs = [...currentWordPairs];
+  shuffleArray(shuffledPairs);
+
+  shuffledPairs.forEach(pair => {
+    const userAnswer = userAnswers[pair.bangla] || ' - ';
+    const li = document.createElement('li');
+    const correct = userAnswer === pair.english;
+    li.innerHTML = `<span>${pair.bangla}</span> <span style="color:${correct ? 'green' : 'red'}">${userAnswer} (${pair.english})</span>`;
+    solutionList.appendChild(li);
+  });
+}
+
+function resetQuiz() {
+  solutionContainer.style.display = 'none';
+  roundResultDiv.textContent = '';
+  quizContainer.style.display = 'flex';
+  initializeQuiz();
+}
+
+// শুরুতেই কুইজ লোড করো
+initializeQuiz();
